47a59d8d5ea96e206e2e19c782b031eeb35f33cc
heaty566 <heaty566@gmail.com>
2021-03-21 13:44:43 +0700

    fix run time

--- index.ts
+++ index.ts
@@ -1,17 +1,17 @@
 //@ts-nocheck
-import puppeteer from "puppeteer";
-import fs from "fs";
-import download from "download";
+import puppeteer from 'puppeteer';
+import fs from 'fs';
+import download from 'download';
 
 async function boot() {
         const browser = await puppeteer.launch({
                 slowMo: 5000,
         });
-        const mapKey = "male";
+        const mapKey = 'male';
 
         const page = await browser.newPage();
         await page.goto(
-                "https://us.wconcept.com/wplp/men/clothing.html?is_first_call=yes&sort=releaseDate+desc&start=0&rows=108"
+                'https://us.wconcept.com/wplp/men/clothing.html?is_first_call=yes&sort=releaseDate+desc&start=0&rows=108'
         );
 
         const items = await page.evaluate(() => {
@@ -30,13 +30,11 @@ async function boot() {
                         if (name) {
                                 data.push({
                                         name: name?.textContent,
-                                        sex: "male",
-                                        price: price?.textContent
-                                                ?.replace("$", "")
-                                                .replace(".00", ""),
-                                        size: ["XS", "S", "M", "L", "XL"],
-                                        color: ["slategrey", "grey", "beige"],
-                                        imageUrl: img?.getAttribute("data-src"),
+                                        sex: 'male',
+                                        price: price?.textContent?.replace('$', '').replace('.00', ''),
+                                        size: ['XS', 'S', 'M', 'L', 'XL'],
+                                        color: ['slategrey', 'grey', 'beige'],
+                                        imageUrl: img?.getAttribute('data-src'),
                                 });
                         }
                 }
@@ -45,24 +43,20 @@ async function boot() {
         });
 
         for (let item = 0; item < items.length; item++) {
-                await download(
-                        `https://us.wconcept.com${items[item].imageUrl}`,
-                        "./images",
-                        {
-                                filename: `image-${mapKey}-${item}.jpg`,
-                        }
-                );
-                items[item].imageUrl = `/images/image-${mapKey}-${item}.jpg`;
+                await download(`https://us.wconcept.com${items[item].imageUrl}`, './output/images', {
+                        filename: `image-${mapKey}-${item}.jpg`,
+                });
+                items[item].imageUrl = `./output/images/image-${mapKey}-${item}.jpg`;
         }
 
         const data = JSON.stringify(items);
 
         // write JSON string to a file
-        fs.writeFile("data.json", data, (err) => {
+        fs.writeFile('./output/data.json', data, (err) => {
                 if (err) {
                         throw err;
                 }
-                console.log("JSON data is saved.");
+                console.log('JSON data is saved.');
         });
 
         await browser.close();
